f1a161fd769e1f0fd4d25f51dd7513ec0bd050ba
Thomas8607 <you@example.com>
2023-05-10 14:06:42 +0200

    clean comment

--- src/fan_rpm.cpp
+++ src/fan_rpm.cpp
@@ -6,7 +6,6 @@
 #include "driver/pcnt.h"
 #include "soc/pcnt_struct.h"
 
-
 #define SIGNAL_INPUT GPIO_NUM_4
 
 TaskHandle_t RpmCalculationHandle = NULL;
@@ -37,36 +36,25 @@ static void IRAM_ATTR pcnt_intr_handler(void *arg) {
 // Initialize PCNT functions for one channel
 void pcnt_init(int16_t PCNT_INPUT_SIG_IO) {
     pcnt_config_t pcnt_config; 
-    // Set PCNT input signal and control GPIOs
     pcnt_config.pulse_gpio_num = PCNT_INPUT_SIG_IO;
     pcnt_config.ctrl_gpio_num = -1;
     pcnt_config.channel = PCNT_CHANNEL_0;
     pcnt_config.unit = PCNT_UNIT_0;
-    // What to do on the positive / negative edge of pulse input?
-    pcnt_config.pos_mode = PCNT_COUNT_INC;   // Count up on the positive edge
-    pcnt_config.neg_mode = PCNT_COUNT_DIS;   // Keep the counter value on the negative edge
-    // What to do when control input is low or high?
-    pcnt_config.lctrl_mode = PCNT_MODE_REVERSE; // Reverse counting direction if low
-    pcnt_config.hctrl_mode = PCNT_MODE_KEEP;    // Keep the primary counter mode if high
-    // Set the maximum and minimum limit values to watch
+    pcnt_config.pos_mode = PCNT_COUNT_INC;
+    pcnt_config.neg_mode = PCNT_COUNT_DIS;
+    pcnt_config.lctrl_mode = PCNT_MODE_REVERSE;
+    pcnt_config.hctrl_mode = PCNT_MODE_KEEP;
     pcnt_config.counter_h_lim = 19;
     pcnt_config.counter_l_lim = -20;
-
-    /* Initialize PCNT unit */
     pcnt_unit_config(&pcnt_config);
-    /* Configure and enable the input filter */
     pcnt_set_filter_value(PCNT_UNIT_0, 100);
     pcnt_filter_enable(PCNT_UNIT_0);
-    /* Enable events on zero, maximum and minimum limit values */
     pcnt_event_enable(PCNT_UNIT_0, PCNT_EVT_ZERO);
     pcnt_event_enable(PCNT_UNIT_0, PCNT_EVT_H_LIM);
-    /* Initialize PCNT's counter */
     pcnt_counter_pause(PCNT_UNIT_0);
     pcnt_counter_clear(PCNT_UNIT_0);
-    /* Register ISR handler and enable interrupts for PCNT unit */
     pcnt_isr_register(pcnt_intr_handler, NULL, 0, &user_isr_handle);
     pcnt_intr_enable(PCNT_UNIT_0);
-    /* Everything is set up, now go to counting */
     pcnt_counter_resume(PCNT_UNIT_0);
 }
 
@@ -80,13 +68,12 @@ int countRPM(uint32_t firstTime, uint32_t lastTime, uint16_t pulsePerRev) {
 }
 // Rpm calculation Task
 void RpmCalculationTask(void *arg) {
-    // Initialize PCNT event queue and PCNT functions
     pcnt_evt_queue = xQueueCreate(10, sizeof(pcnt_evt_t));
     pcnt_init(SIGNAL_INPUT);                                                    // Initialize PCNT
     uint32_t lastStamp = 0;                                                     // for previous timestamp
     pcnt_evt_t evt;
     portBASE_TYPE res;
-    for(;;){
+    while(1) {
         res = xQueueReceive(pcnt_evt_queue, &evt, 1000 / portTICK_PERIOD_MS);
         if (res == pdTRUE) {
             if (lastStamp == 0) {
@@ -100,6 +87,8 @@ void RpmCalculationTask(void *arg) {
         }
     }
 }
+
+
 
 void setup() {
     Serial.begin(115200);
